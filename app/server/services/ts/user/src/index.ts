ecaa139b17018379423689c4f61fcc16604a49d0
Logan <logand007@outlook.com>
2026-02-11 14:40:49 -0600

    factored out some common methods

--- app/server/services/ts/user/src/index.ts
+++ app/server/services/ts/user/src/index.ts
@@ -1,13 +1,16 @@
 import { PORT } from '@/port';
+import { onExit } from '@/hooks';
+import { buildCorsConfig } from '@/corsUtil';
 import express from 'express';
-const app = express();
 
+const app = express();
 app.use(express.json());
+app.use(buildCorsConfig());
 
 const server = app.listen(PORT, () => {
   console.log(`User Service running on port ${PORT}`);
 });
-process.on("SIGTERM", () =>  server.close());
+onExit(() => server.close());
 
 //test endpoint
 app.get('/health', (req: express.Request, res: express.Response) => {
