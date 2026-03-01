e610dd05b326a9b55912a474fe4c2ac530c234c5
Jackie Mei <meij1@myumanitoba.ca>
2026-02-28 23:03:50 -0600

    Created components to create a list of user's income with cards.

---
+++ app/client/src/components/ui/IncomeList.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import { getIncome, type Income } from "../../api/Income"
+import IncomePopup from "./IncomeForm"
+import IncomeCard from "./IncomeCard"
+
+export default function IncomeList() {
+
+    const [userIncomes, setUsersIncomes] = useState<Income[]>([])
+    const [seen, setSeen] = useState<boolean>(false)
+
+    
+        //Try to get the account's transaction from the server
+        try {
+            getIncome().then((incomes)=>{
+                //Determine if we acquired the accounts transaction 
+                if(incomes) {
+                    setUsersIncomes(incomes)
+                }
+            })
+        } catch(error) {
+    
+        }
+        
+        //Adds a account to the list
+        const addIncome = (newIncome:Income) => {
+            setUsersIncomes(userIncomes => [...userIncomes,newIncome])
+        }
+    
+        //Removes any income from the list thats shares the same id
+        const removeIncome = (remove:Income) => {
+            setUsersIncomes(userIncomes=> userIncomes.filter((incomes) => incomes.id !== remove.id))
+        }
+    
+        const toggle = () => {
+            setSeen(!seen)
+        }
+    
+        return(
+            <>
+            <div>
+                <div>
+                {userIncomes.map(income => (<IncomeCard income={income} setIncome={addIncome} removeIncome={removeIncome}/>))}
+                </div>
+    
+                <div>
+                    <button onClick={toggle}>Add Income</button>
+                </div>
+            </div>
+    
+            {seen ? (<IncomePopup toggle={toggle} addIncome={addIncome} edit = {false} />):null}
+            </>
+        )
+}
