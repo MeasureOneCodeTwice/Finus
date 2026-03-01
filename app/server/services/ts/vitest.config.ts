c791ae0d290bdcb206d4d876210f3b0c930f9dc7
Logan <logand007@outlook.com>
2026-02-28 18:40:48 -0600

    got auth tests running in dockerfile

--- app/server/services/ts/vitest.config.ts
+++ app/server/services/ts/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 import path from "path"
 
 export default defineConfig({
-    resolve : {
+    resolve: {
         alias: {
             "@": path.resolve(__dirname, "./common")
         }
