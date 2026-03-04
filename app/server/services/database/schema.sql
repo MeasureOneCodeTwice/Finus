bc5004eec9781ed09fb6667b75a9fc1cb9e29b22
Roman Tebel <tebelroman@gmail.com>
2026-03-04 13:39:37 -0600

    changed the db granting script text endings to LF type so that they work on Docker

--- app/server/services/database/schema.sql
+++ app/server/services/database/schema.sql
@@ -165,4 +165,4 @@ INSERT INTO finus.financialAccountSubtype (subtype) VALUES ('RRSP'), ('TFSA'), (
 INSERT  INTO finus.investmentType          (type) VALUES ('fixedInterest'), ('stock'); #These have to match table names
 INSERT  INTO finus.goalType                (type) VALUES ('money'), ('debt');
 
-GRANT ALL PRIVILEGES ON finus.* TO 'finus_app'@'%';
+-- GRANT ALL PRIVILEGES ON finus.* TO 'finus_app'@'%';
