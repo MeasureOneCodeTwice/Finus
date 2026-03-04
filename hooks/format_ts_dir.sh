96a94f2938c5277f81b920beb18b29a25164b852
Logan <logand007@outlook.com>
2026-03-04 14:58:56 -0600

    improve pre-commit hook script

--- hooks/format_ts_dir.sh
+++ hooks/format_ts_dir.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #first argument ($1) is the directory to format the files of.
-changed_files=$(git diff --cached --name-only --diff-filter=ACMR | grep -E $1/)
-if [ -n $changed_files ]; then
+changed_files=$(git diff --cached --name-only --diff-filter=ACMRd | grep -E $1/)
+if [ -z "$changed_files" ]; then
     exit 0
 fi
-echo "files: $changed_files"
 
 normalized_files=$(echo $changed_files | sed "s|$1/||g" )
 cd $1
-bun eslint $changed_dir_files
+bun eslint $normalized_files
 if [[ $? != 0 ]]; then
     echo "❌ Failed to format files"
     exit 1
 fi
-bun prettier $changed_dir_files --write --ignore-unknown
+bun prettier $normalized_files --write --ignore-unknown
+
 git update-index --again
 
