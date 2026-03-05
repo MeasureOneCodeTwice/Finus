6c7c0d774be83191b0e100e104b6a9fa4628ac80
Logan <logand007@outlook.com>
2026-03-04 19:43:57 -0600

    fix cors issue when running frontent via bun

--- app/server/services/ts/common/expressUtils.ts
+++ app/server/services/ts/common/expressUtils.ts
@@ -1,13 +1,13 @@
 import cors from "cors";
 
 const defaultMethods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"];
-const defaultOrigin = "http://localhost";
+const defaultOrigins = ["http://localhost:80", "http://localhost:8080"];
 export function buildCorsConfig(opts?: {
-  origin?: string;
+  origins?: string | string[];
   methods?: string[];
 }) {
   return cors({
-    origin: opts?.origin ?? defaultOrigin,
+    origin: opts?.origin ?? defaultOrigins,
     methods: opts?.methods ?? defaultMethods,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
