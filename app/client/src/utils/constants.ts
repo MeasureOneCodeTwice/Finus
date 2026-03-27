1a7a8695f1c3308a4ed95de27fa242ddd0b5119a
Logan <logand007@outlook.com>
2026-03-27 11:15:16 -0500

    localhost -> api_gateway_url in config

--- app/client/src/utils/constants.ts
+++ app/client/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = process.env.API_GATEWAY_URL;
 export const SESSION_STORAGE_KEY = "finus-session";
 export const SIDEBAR_WIDTH = 260;
