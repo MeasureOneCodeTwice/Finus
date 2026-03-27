b8cba725109c5e7e52d701b7ea704129efd87490
Logan <logand007@outlook.com>
2026-03-26 19:15:09 -0500

    get docker compose in order file client

--- app/client/vitest.config.ts
+++ app/client/vitest.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
-  test: {
-    include: ["test/**/*.test.ts"],
-    globals: true,
-    environment: "jsdom",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
